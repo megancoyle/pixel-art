3172eaa1fbf9dc9f3402f0170613b42ee2b5540a
Megan Coyle <meg@mcoyle.com>
2016-04-07 15:22:55 -0400

    Add initial pixel art files

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # pixel-art
 Create pixel art with JavaScript
+
+This was an in-class assignment in GA that I decided to add a couple more pieces of functionality to.
+
+Uses <a href="https://github.com/niklasvh/html2canvas">html2canvas</a> to take a screenshot of your pixel creation.
---
+++ js/pixelart.js
@@ -0,0 +1,34 @@
+// Set variables for jQuery for pixel arting
+var $button = $("#set-color");
+var $brush = $(".brush");
+var $input = $("#color-field");
+var $form = $("#form");
+var $body = $("body");
+var $art = $("#art");
+
+// Set color to use
+$button.on("click", function (e) {
+    e.preventDefault();
+    var color = $input.val();
+    $brush.css("background", color);
+})
+
+// Creates pixel art canvas
+for (var i = 0; i < 5000; i++) {
+$art.append("<div class='square'></div>");
+}
+
+// Gives user ability to draw on canvas
+$(".square").on("mouseover", function (event){
+  var $square = $(event.target);
+  $square.css("background", $input.val())
+})
+
+// JavaScript for Taking Screenshot of the Page
+    $("#btnSave").click(function() {
+      html2canvas(document.$art, {
+      onrendered: function(canvas) {
+      document.body.appendChild(canvas);
+    }
+  });
+});
